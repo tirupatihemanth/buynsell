318af6405dd14fa2e06266bd3fa7cfdae3e57e6c
punit-mahipal <punit25mahipal@gmail.com>
2014-09-26 01:28:05 +0530

    seventh commit
    rename header

--- header1.php
+++ header1.php
@@ -1,192 +1,34 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<!-- Latest compiled and minified CSS -->	
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<?php
+echo "
+  <style>
+  ul#header{
+       }      
+   ul#header li{
+    font-size:20px;
+    display : inline;
+   }
+   ul#header li a{
+    background-color: blue;
+    color:white;
+    padding:10px 20px;
+	border:1px solid black;
+    border-radius: 0px 0px 5px 5px;
+   }
+   ul#header li a:hover{
+    background-color: green;
+   }
+   </style>
+  <html>
+    <ul id = 'header'>
+    <li><a href = 'afterlogin.php'>Home</a></li>
+    <li><a href = 'newpost.php'>New Post</a></li>
+    <li><a href = 'posts.php?myposts=1'>My Posts</a></li>
+    <li><a href = 'posts.php?allposts=1'>All Posts</a></li>
+    <li><a href = 'updateprofile.php'>Update Profile</a></li>
+    <li><a href = 'logout.php'>Signout</a></li>
+    </ul>
+  </html>
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+";
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-		
-		<style>
-			.dropdown:hover .dropdown-menu {
-							display: block;
-							}
-			.nav-bottom{
-						background-color: #a0a0a0;
-						margin-bottom: 0px;
-						}
-			
-		</style>
-	</head>
-	
-
-	<body>
-		<nav class="navbar navbar-default nav-bottom" role="navigation">
-			<div class="col-xs-12 col-md-12">
-				<div class="col-xs-1 col-md-1">
-					<a href="posts.php?myposts=1">
-						<button type="button" class="btn btn-default btn-lg">
-							<span class="glyphicon glyphicon-star"></span> 
-							IIT-M Tag
-						</button>	
-					</a>		
-				</div>
-			
-				<div class="col-xs-10 pull-right">
-					<div  class="col-xs-12 col-md-12 ">
-						<div class="col-xs-6 col-md-6">
-							<input type="text" class ="input-lg col-xs-12" placeholder ="    Search">
-						</div>
-					
-						<div class="col-xs-6 pull-right" >
-							<a href="newpost.php">
-							<button type="button" class="col-xs-8 btn btn-warning btn-lg pull-right">POST an AD</button>
-							</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-xs-12">
-					<br>
-				</div>
-			</div>		
-		</nav>
-		
-		<nav class="navbar navbar-default" role="navigation">
-			<div  class="col-xs-12">
-				<div class=" col-xs-9 ">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<div class="nav nav-pills menu-links">
-							<ul class="nav navbar-nav pull-left">
-							
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">ELETRONICS<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>	
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">ACCESSORIES <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>	
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">MEN <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">WOMEN <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>	
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">BOOKS <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>	
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">VEHICLES<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>				
-								 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">MORE <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-		
-								
-								
-							</ul>
-						</div>
-					</div>
-				</div>
-				
-				<div class="col-xs-1">
-				</div>
-				
-				<div class="col-xs-1">
-					<button class="btn btn-default btn-lg  dropdown-toggle"  type="button"  data-toggle="dropdown">
-						MY ACCOUNT
-						<span class="caret"></span> <!-- dropdown's arrow-->
-					</button>
-					<ul class="dropdown-menu"  role="menu" >
-						<li role="presentation"><a role="menuitem"  href="afterlogin.php">My Profile</a></li>
-						<li role="presentation"><a role="menuitem"  href="posts.php?myposts=1">My Posts</a></li>
-						<li role="presentation"><a role="menuitem"  href="updateprofile.php">Setting</a></li>
-						<li role="presentation"><a role="menuitem"  href="#">Suggestions</a></li>
-						<li role="presentation" class="divider"></li>
-						<li role="presentation"><a role="menuitem"  href="logout.php"> Logout</a></li>
-					</ul>
-				</div>
-				
-			</div>
-		</nav>		
-	
-		
-	</body>
-	
-
-</html>
+?> 
