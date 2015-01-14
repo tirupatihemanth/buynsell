cbff271425c9f5ad4ac47aadfdf141cc611f32b9
hvvivek <hvvivek2007@gmail.com>
2015-01-14 23:18:39 +0530

    Frontend

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
     //This file contains some of the constants that I will be frequently using related to the database
     $WEBHOST = "localhost";
     $USER = "root";
-    $PASSWORD = "a";
+    $PASSWORD = "";
     $DATABASE = "buynsell";
     
 ?>
